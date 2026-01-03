d184321bd9a65df387c4b6305622f2da29f5e988
thdeji <omotobaadedeji@gmail.com>
2026-01-04 00:47:08 +0100

    added loading animation to improve UX

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,11 @@ function App() {
   const [advice, setAdvice] = useState("");
   const [title, setTitle] = useState("");
   const [generate, setGenerate] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
 
   const getAdvice = async () => {
+    setIsLoading(true);
+
     try {
       const res = await fetch("https://api.adviceslip.com/advice");
       const data = await res.json();
@@ -14,12 +17,10 @@ function App() {
       setTitle(data.slip.id);
     } catch (error) {
       console.error("Error fetching advice:", error);
+    } finally {
+      setTimeout(() => setIsLoading(false), 500);
     }
   };
-
-  useEffect(() => {
-    getAdvice();
-  }, [{ generate }]);
 
   const isNotMobile = window.innerWidth > 475;
 
@@ -28,22 +29,24 @@ function App() {
       <div className="bg-blue900 p-10 flex flex-col gap-5 text-center mx-10 md:mx-50  rounded-lg shadow-2xl shadow-black">
         <h1 className="text-green-300 text-sm">ADVICE #{title}</h1>
         <p className="text-blue200 text-2xl font-bold">"{advice}"</p>
-        {isNotMobile ? <img
-          src="./images/pattern-divider-desktop.svg"
-          alt="pattern divider"
-        />: <img
-          src="./images/pattern-divider-mobile.svg"
-          alt="pattern divider"
-        />}
+        {isNotMobile ? (
+          <img
+            src="./images/pattern-divider-desktop.svg"
+            alt="pattern divider"
+          />
+        ) : (
+          <img
+            src="./images/pattern-divider-mobile.svg"
+            alt="pattern divider"
+          />
+        )}
       </div>
       <div
         className="-mt-4 bg-green-300 p-4 rounded-full cursor-pointer hover:shadow-[0_0_20px_5px_var(--color-green-300)] transition-all duration-300"
-        onClick={() => setGenerate((prev) => !prev)}
+        onClick={getAdvice}
       >
         <img
-          className={`transition-transform duration-500 ease-in-out ${
-            generate ? "rotate-180" : "rotate-0"
-          }`}
+          className={`size-6 ${isLoading ? "animate-rolling" : ""}`}
           src="./images/icon-dice.svg"
           alt="icon-dice.svg"
         />
